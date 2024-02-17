992491a68982d8d056b78db3c3412e82d33a74bc
Rishabh <chaudharyrishabh0210@gmail.com>
2024-02-17 20:57:30 +0530

    added clerk-auth prisma-supabase initialised

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+
+const Layout = ({children}:{children : React.ReactNode})=>{
+    return <div className="min-h-screen flex items-center justify-center bg-red-400">
+        {children}
+    </div>
+}
+export default Layout;
